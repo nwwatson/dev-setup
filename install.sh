d8f6fb86eaf1993913156cf3e73aeb06735d7031
Nicholas Watson <nwwatson@Nicks-Mac-mini.local>
2026-03-03 13:35:15 -0500

    updated ghostty config

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 
 brew install tmux
 brew install starship
+brew install --cask ghostty
+brew install neovim
 
-mkdir -p ~/.config && cp ./starship/starship.toml ~/.config/starship.toml
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+
+curl -fsSL https://claude.ai/install.sh | bash
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc && source ~/.zshrc
+
+source ~/.zshrc
+
+mkdir -p ~/.config/starship && cp ./starship/starship.toml ~/.config/starship.toml
+mkdir -p ~/.config/ghostty && cp ./ghostty/config ~/.config/ghostty/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
